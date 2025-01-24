bef8fd1b0ab70858893c3fafd746b23ca5f6183c
Katelyn Stearn <krstearn@gmail.com>
2025-01-24 14:58:27 -0500

    Budget and Spending pages

--- src/routes/Budget.jsx
+++ src/routes/Budget.jsx
@@ -1,8 +1,46 @@
 const Budget = () => {
     return (
-        <div>
+        <div className="budget">
             <h2>Budget</h2>
             <p>Welcome to the budget</p>
+            <div className="budgetTableContainer">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Category</th>
+                            <th>Budget</th>
+                            <th>Spent</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Food</td>
+                            <td>$300</td>
+                            <td>$250</td>
+                        </tr>
+                        <tr>
+                            <td>Transportation</td>
+                            <td>$100</td>
+                            <td>$50</td>
+                        </tr>
+                        <tr>
+                            <td>Entertainment</td>
+                            <td>$200</td>
+                            <td>$150</td>
+                        </tr>
+                        <tr>
+                            <td>Health</td>
+                            <td>$150</td>
+                            <td>$100</td>
+                        </tr>
+                        <tr>
+                            <td>Other</td>
+                            <td>$200</td>
+                            <td>$100</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/routes/Spending.jsx
+++ src/routes/Spending.jsx
@@ -1,8 +1,12 @@
 const Spending = () => {
     return (
-        <div>
+        <div className="spending">
             <h2>Spending</h2>
             <p>Welcome to the spending</p>
+            <div className="spendingDataContainer">
+                <div className="spendingChartContainer">f</div>
+                <div className="spendingTableContainer">f</div>
+            </div>
         </div>
     );
 };
